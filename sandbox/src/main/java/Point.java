f561c9b8625ae6030c6adcddd5a8dad5ea3a31e5
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-08-04 16:22:35 +0300

    Домашнее задание 2

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,18 @@
+public class Point {
+    double x;
+    double y;
+    double x2;
+    double y2;
+
+    public Point(double x, double y, double x2, double y2) {
+        this.x = x;
+        this.y = y;
+        this.x = x2;
+        this.y = y2;
+    }
+
+    public static double distance(double x, double y, double x2, double y2){
+        double pow = (Math.pow(x2 - x, 2)) + (Math.pow(y2 - y,2));
+        return Math.sqrt(pow);
+    }
+}
