839ab3836785584710879ddea7b632509fdd7956
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-08-16 21:58:19 +0300

    ДЗ 7: добавил браузеры IE,Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,39 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
+    private int browser;
+
+    public ApplicationManager(int browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        if (browser == BrowserType.MOZILLA) {
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+            wd = new InternetExplorerDriver();
+        } else if (browser == BrowserType.DEFAULT) {
+            wd = new ChromeDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
