f03c6731f741470429121b5667d307bc8fdccd99
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-08-17 18:05:38 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
@@ -11,10 +10,18 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("GROUPS")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
        click(By.linkText("GROUPS"));
     }
 
     public void goToAddContact() {
+        if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("EDIT_ADD_ENTRY")) {
+            return;
+        }
         clickContact(By.linkText("ADD_NEW"));
     }
 }
