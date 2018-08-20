3f74e4b50b04ec20b27517cede5b94de90244935
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-08-20 20:33:56 +0300

    ДЗ-7(Правки ): Запуск тестов на разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,32 +5,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    WebDriver wd;
+    private WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
-    private int browser;
+    private String browser;
 
-    public ApplicationManager(int browser) {
-
+    public ApplicationManager(String browser) {
         this.browser = browser;
     }
 
     public void init() {
-        if (browser == BrowserType.MOZILLA) {
+        if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
-        } else if (browser == BrowserType.DEFAULT) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
         }
 
