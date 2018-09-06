11c13f64004d8c198d0fd5661fbea56cb489b8e6
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-09-06 22:17:13 +0300

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,22 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        File photo = new File("src/test/resources/img.png");
+        list.add(new Object[] {new ContactData().withFirstname("Sergey").withLastname("Tambolsky").withPhoto(photo).withAddress("Belarus").withEmail("test@test.test").withEmail2("test2@test.test").withEmail3("test3@test.test").withHomePhone("+17512345678").withMobilePhone("+18387487934").withWorkPhone("+14298653676")});
+        list.add(new Object[] {new ContactData().withFirstname("Sergey2").withLastname("Tambolsky2").withPhoto(photo).withAddress("Belarus2").withEmail("test@test.test2").withEmail2("test2@test.test2").withEmail3("test3@test.test2").withHomePhone("+237512345678").withMobilePhone("+28387487934").withWorkPhone("+24298653676")});
+        list.add(new Object[] {new ContactData().withFirstname("Sergey3").withLastname("Tambolsky3").withPhoto(photo).withAddress("Belarus3").withEmail("test@test.test3").withEmail2("test2@test.test3").withEmail3("test3@test.test3").withHomePhone("+37512345678").withMobilePhone("+38387487934").withWorkPhone("+34298653676")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.contact().goToHome();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/img.png");
-        ContactData contact = new ContactData().withFirstname("Sergey").withLastname("Tambolsky").withPhoto(photo).withAddress("test123").withHomePhone("+37512345678").withMobilePhone("+68387487934").withWorkPhone("+74298653676").withEmail("test@test.test").withEmail2("test2@test.test").withEmail3("test3@test.test");
         app.contact().goToAddContact();
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
