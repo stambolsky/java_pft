756a969a19fa88b13d83dd7a5cf25ac5ae082f99
Sergey Tambolsky <42043645+stambolsky@users.noreply.github.com>
2018-09-29 20:38:26 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("33d362696222c61f32ba09b25831adf4933a8419");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("stambolsky", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+
+}
